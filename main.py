6fb1bd081d329a854db8b6d53ee9d80b5fc0add3
Andrey <siberianhunger@gmail.com>
2024-06-10 01:00:05 +0300

    CREATE CACHING WITH REDIS TO ADD MESSAGE HISTORY

--- main.py
+++ main.py
@@ -1,59 +1,103 @@
 import os
-from groq import Groq
-import logging
-
+from groq import AsyncGroq
+from groq.types.chat import ChatCompletionMessage
 from telegram import Update
 from telegram.ext import (
     Application,
-
     CommandHandler,
     ContextTypes,
 )
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+from models import Roles
+from logger import logger
+from redis_client import get_cached_messages, set_messages_in_cache
 
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+groq_client = AsyncGroq(api_key=os.environ.get("GROQ_API_KEY"))
 
-logger = logging.getLogger(__name__)
-groq_client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
-)
 model: str = os.environ.get("MODEL") or "llama3-70b-8192"
 
 
-async def llama_call(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_to_reply = update.message.from_user.name or ''
-    localized_question = f"Ответь на русском. {update.message.text.replace('/llama', '')}"
-    generated_msg = await query_groq_for_data(request_sentence=localized_question)
-    logger.info(f'localized_question: {localized_question}')
-    msg_to_send = f"{user_to_reply}\n{generated_msg}"
+def prompt_msg_builder(role: Roles, content: str):
+    return {"role": role, "content": content}
+
+
+default_system_prompts = (
+    prompt_msg_builder(Roles.system, "If your answer contains any codeblocks use MarkdownV2 to ident it."),
+    prompt_msg_builder(Roles.system,
+                       "If your answer doesn't have any code or some config files don't use MarkdownV2."),
+    prompt_msg_builder(Roles.system, "Don't use chinese characters, only Russian and English is allowed."),
+    prompt_msg_builder(Roles.system, 'Ответь на русском.'),
+)
+
+eng_system_prompts = (
+    prompt_msg_builder(Roles.system, "If your answer contains any codeblocks use MarkdownV2 to ident it."),
+    prompt_msg_builder(Roles.system,
+                       "If your answer doesn't have any code or some config files don't use MarkdownV2."),
+    prompt_msg_builder(Roles.system, "Don't use chinese or Russian characters, only English is allowed."),
+)
+
+
+async def llama_with_context(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    cache_key = f"{update.message.chat_id}_{update.message.from_user.name}_cache"
+    parsed_question = update.message.text.replace('/llama', '')
+    logger.info(f'new question!: {parsed_question}')
+    cached_msgs = await get_cached_messages(cache_key)
+    if cached_msgs:
+        logger.info(f'conversation with cache!')
+        msg_list = [*cached_msgs, prompt_msg_builder(Roles.user, parsed_question)]
+        logger.info(f'full msg list: {msg_list}')
+        generated_msg = await query_groq_for_data(msg_list)
+        await set_messages_in_cache(cache_key, [*msg_list, prompt_msg_builder(Roles.assistant, generated_msg.content)])
+    else:
+        logger.info(f'no cache for conversation found!')
+        generated_msg = await query_groq_for_data([prompt_msg_builder(Roles.user, parsed_question)])
+        await set_messages_in_cache(cache_key, [prompt_msg_builder(Roles.assistant, generated_msg.content)])
+    msg_to_send = f"{update.message.from_user.name}\n{generated_msg.content}"
     await update.effective_message.reply_text(msg_to_send)
 
 
-async def query_groq_for_data(request_sentence: str) -> str:
-    chat_completion = groq_client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": request_sentence,
-            }
-        ],
+async def lleng(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    cache_key = f"{update.message.chat_id}_{update.message.from_user.name}_eng_cache"
+    parsed_question = update.message.text.replace('/llama', '')
+    logger.info(f'new question!: {parsed_question}')
+    cached_msgs = await get_cached_messages(cache_key)
+    if cached_msgs:
+        logger.info(f'conversation with cache!')
+        msg_list = [*cached_msgs, prompt_msg_builder(Roles.user, parsed_question)]
+        logger.info(f'full msg list: {msg_list}')
+        generated_msg = await query_groq_for_data(msg_list, eng_system_prompts)
+        await set_messages_in_cache(cache_key, [*msg_list, prompt_msg_builder(Roles.assistant, generated_msg.content)])
+    else:
+        logger.info(f'no cache for conversation found!')
+        generated_msg = await query_groq_for_data([prompt_msg_builder(Roles.user, parsed_question)], eng_system_prompts)
+        await set_messages_in_cache(cache_key, [prompt_msg_builder(Roles.assistant, generated_msg.content)])
+    msg_to_send = f"{update.message.from_user.name}\n{generated_msg.content}"
+    await update.effective_message.reply_text(msg_to_send)
+
+
+async def llama_ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    parsed_question = update.message.text.replace('/llask', '')
+    logger.info(f'llama_ask: {parsed_question}')
+    generated_msg = await query_groq_for_data([prompt_msg_builder(Roles.user, parsed_question)])
+    msg_to_send = f"{update.message.from_user.name}\n{generated_msg.content}"
+    await update.effective_message.reply_text(msg_to_send)
+
+
+async def query_groq_for_data(user_prompts: list[dict], sys_promts=default_system_prompts) -> ChatCompletionMessage:
+    chat_completion = await groq_client.chat.completions.create(
+        messages=[*sys_promts, *user_prompts],
         model=model,
     )
-
-    return chat_completion.choices[0].message.content
+    return chat_completion.choices[0].message
 
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(os.environ.get("TG_BOT_TOKEN")).build()
-
-    application.add_handler(CommandHandler("llama", llama_call))
-
+    application.add_handler(CommandHandler("llama", llama_with_context))
+    application.add_handler(CommandHandler("llask", llama_ask))
+    application.add_handler(CommandHandler("lleng", lleng))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
---
+++ redis_client.py
@@ -0,0 +1,32 @@
+import aioredis
+import json
+import os
+
+from logger import logger
+
+
+redis_pool = aioredis.from_url(
+    f'redis://{os.environ.get("redis_host") or "localhost"}',
+    password=os.environ.get("redis_host") or 'pepe123',
+    port=6379,
+    decode_responses=True
+)
+
+
+async def get_cached_messages(key: str) -> list[dict] | None:
+    try:
+        raw_data = await redis_pool.get(key)
+        if raw_data:
+            logger.info(f'get_cached_messages {raw_data}')
+            return json.loads(raw_data)
+    except Exception as exc:
+        logger.info(f"Exception while trying to get cached msgs for key: {key} exc: {exc}")
+        return
+
+
+async def set_messages_in_cache(key: str, raw_data: list[dict]) -> None:
+    try:
+        data = json.dumps(raw_data)
+        await redis_pool.set(key, data, ex=20 * 60)
+    except Exception as exc:
+        logger.info(f"Exception while trying to set cached msgs for key: {key} exc: {exc}")
